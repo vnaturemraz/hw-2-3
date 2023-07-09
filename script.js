5423639c8913efca62696e96997171a5f07b22b1
vnaturemraz <vnaturemrazz@gmail.com>
2023-07-10 00:30:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//1
+//let password = 'пароль';
+//let userPassword = prompt('Введите пароль');
+//password==userPassword ? console.log ('Пароль введен верно') : console.log('Пароль введен неправильно');
+
+//2
+//let c = prompt('введите число');
+//c>0 && c<10 ? console.log('верно') : console.log('неверно');
+
+//3
+//let d = prompt('введите число 1');
+//let e = prompt('введите число 2');
+//d>100 || e>100 ? console.log ('верно') : console.log ('неверно');
+
+//4
+//let a = '2';
+//let b = '3';
+//alert((Number(a))+(Number(b)));
+
+//5
+//let monthNumber = prompt(String('введите номер месяца'));
+
+//switch (monthNumber){
+  //  case '12':
+  //  case '1':   
+//    case '2':
+  //      alert('Зима');
+    //    break;
+    
+   // case '3':
+    //case '4':
+    //case '5':
+    //    alert('Весна');
+    //    break;
+
+   // case '6':
+    //case '7':
+    //case '8':
+      //  alert('Лето');
+        //break;
+       
+        //  case '9':
+          //case '10':
+          //case '11':
+            //  alert('Осень');
+             // break;
+      
+         // default: alert('такого месяца нет');
+           //   break;
+      
+      //}
+      
